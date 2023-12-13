8ff53f034155741ab21da5adbb9f93904c3a1e2d
Jacob Kueck <jacob.kueck@gmail.com>
2023-12-12 18:13:32 -0600

    Everything i have added so far

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES  ("Bakery"),
+        ("Dairy"),
+        ("Produce"),
+        ("Frozen"),
+        ("Meat"),
+        ("Deli");
+
+INSERT INTO roles (title)
+VALUES  ("Baker"),
+        ("Dairy Employee"),
+        ("Produce Employee"),
+        ("Frozen Employee"),
+        ("Butcher"),
+        ("Deli Slicer");
+
+INSERT INTO employee (first_name)
+VALUES 
